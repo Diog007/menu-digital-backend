fa6cc62e7e2891cf4cde99c608a9133d5280c88e
Diog007 <diogo.mz123@gmail.com>
2024-02-05 17:41:24 -0300

    Created method to list foods

--- src/main/java/com/card/menu/controller/FoodController.java
+++ src/main/java/com/card/menu/controller/FoodController.java
@@ -1,16 +1,27 @@
 package com.card.menu.controller;
 
+import com.card.menu.food.Food;
+import com.card.menu.food.FoodRepository;
+import com.card.menu.food.FoodResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("food")
 public class FoodController {
 
-    @GetMapping
-    public void getAll(){
+    @Autowired
+    private FoodRepository repository;
 
+    @GetMapping
+    public List<FoodResponseDTO> getAll(){
+
+        List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
+        return foodList;
     }
 
 
